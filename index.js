3006c8aef740e40ba773373201e002842b2a98ee
Ruth Melissa <ruthy9403@gmail.com>
2021-09-14 19:29:20 -0500

    week funcionality

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get('/', (req, res) =>{
     res.send('Hello World');
 });
 
+app.get('/day', (req, res) => {
+    days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    date = new Date()
+    res.send(`Day is ${days[ date.getDay() ]}`)
+  });
 
 app.listen(port, () =>{
     console.log(`Port is UP!, running on port ${port}`)
